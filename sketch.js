6af1f9a03ce8fb5f008b094411f6e0f76d189669
PriyankaDathriB <priyanka.dathri@gmail.com>
2024-07-09 10:28:06 +0530

    Intro to angry birds

---
+++ sketch.js
@@ -0,0 +1,21 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var myEngine, myWorld;
+
+
+
+function setup(){
+    var canvas = createCanvas(400,400);
+    myEngine = Engine.create();
+    myWorld = myEngine.world;
+    
+    
+}
+
+function draw(){
+    background(0);
+    Engine.update(myEngine);
+    
+}
